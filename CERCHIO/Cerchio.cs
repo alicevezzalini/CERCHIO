dcfcacf205933b25ca44a67150c034fc430f62ea
Utente <Utente@PC-DI-SIMO>
2024-01-28 16:15:42 +0100

    Aggiungere i file di progetto.

---
+++ CERCHIO/Cerchio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CERCHIO
+{
+    internal class Cerchio
+    {
+        public int Raggio { get; set; }
+
+        public Cerchio(int raggio) 
+        { 
+            Raggio = raggio;         
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Cerchio R = {0}", Raggio);
+        }
+
+       
+    }
+}
---
+++ CERCHIO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CERCHIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c = new Cerchio(6);
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
